8573ca34e4d28fed6f707d51f27c15362841077f
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2018-04-09 09:37:14 +0530

    First commit

---
+++ BlazorCrud/BlazorCrud.Server/DataAccess/EmployeeContext.cs
@@ -0,0 +1,22 @@
+﻿using BlazorCrud.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Server.DataAccess
+{
+    public class EmployeeContext : DbContext
+    {
+        public virtual DbSet<Employee> tblEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=myTestDB;Data Source=ANKIT-HP;");
+            }
+        }
+    }
+}
